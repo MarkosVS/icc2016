1bd0a0a44fceff961c213ab67b10efd32467cf4b
MarkosVS <markos.vs@live.com>
2016-09-05 10:51:14 -0300

    Add files via upload

---
+++ exercicio0509.c
@@ -0,0 +1,10 @@
+/*
+1- Escreva um programa que permite ao usuário entrar com:
+    - um montante em dinheiro
+    - número de períodos
+    - juros por período
+Seu programa deve calcular o total devido ao final do período
+
+
+
+*/
